0a0d6eb7f0e02200d8814167006332bb5ddd06a6
njorquera98 <njorquera98.dev@gmail.com>
2023-11-22 08:55:00 -0300

    log in

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,69 +1,40 @@
-
-  <template>
-    <div class="q-pa-md">
-      <div class="q-gutter-y-md column" style="max-width: 300px">
-        <q-field color="purple-12" label="Label" stack-label>
-          <template v-slot:prepend>
-            <q-icon name="event" />
-          </template>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-          </template>
-        </q-field>
-
-        <q-field color="teal" filled label="Label" stack-label>
-          <template v-slot:prepend>
-            <q-icon name="event" />
-          </template>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-          </template>
-        </q-field>
-
-        <q-field color="grey-3" label-color="orange" outlined label="Label" stack-label>
-          <template v-slot:append>
-            <q-icon name="event" color="orange" />
-          </template>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-          </template>
-        </q-field>
-
-        <q-field color="lime-11" bg-color="green" filled label="Label" stack-label>
-          <template v-slot:prepend>
-            <q-icon name="event" />
-          </template>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-          </template>
-        </q-field>
-
-        <q-field color="teal" outlined label="Label" stack-label>
-          <template v-slot:append>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
-            </q-avatar>
-          </template>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-          </template>
-        </q-field>
-
-        <q-field color="orange" standout bottom-slots :model-value="text" label="Label" stack-label counter clearable>
-          <template v-slot:prepend>
-            <q-icon name="place" />
-          </template>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-          </template>
-          <template v-slot:append>
-            <q-icon name="favorite" />
-          </template>
-
-          <template v-slot:hint>
-            Field hint
-          </template>
-        </q-field>
-      </div>
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-field label="Usuario" stack-label>
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
+        </template>
+      </q-field>
     </div>
-  </template>
+
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Password with toggle">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      text: ref(''),
+      password: ref(''),
+      isPwd: ref(true),
+    }
+  }
+}
+</script>
